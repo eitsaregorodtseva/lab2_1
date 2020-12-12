b19268e5afb9ec876d072c4d64c6f38edc352b18
jimixisshit <61564289+eitsaregorodtseva@users.noreply.github.com>
2020-12-12 11:21:07 +0300

    Update mapcollection.h

--- mapcollection.h
+++ mapcollection.h
@@ -6,23 +6,25 @@
 
 template<typename K, typename V> class MapCollection {
 public:
-    MapCollection();                                      //по умолчанию
-    MapCollection(const MapCollection &other_);           //копирования
-    void clear();                                         //удаляющий все значения
-    size_t size() const;                                  //возвращающий количество
-    void readFromFile(std::string filename);              //из файла
-    void writeToFile(std::string filename) const;         //в файл ДОПИСАТЬ
-    bool operator==(const MapCollection &other_);   //оператор ==
-    bool compareMaps(const MapCollection &other_);  //для == ДОПИСАТЬ
-    void insert(const K& key_, const V& value_);          //добавляющий новую пару
-    void erase(const K& key_);                            //удаляющий пару
-    V &find(const K& key_);                               //для []
-    V &operator[](const K& key_);                         //оператор []
-    bool exist(const K& key_) const;                      //проверка ключа
-    std::vector<V> allValuesReturn(const MapCollection &other_);               //ДОПИСАТЬ
-    bool compareKeys(const K& key_);                        //для > ПРОВЕРИТЬ
-    bool operator>(const K& key_);                          //оператор > ПРОВЕРИТЬ
-    //MapCollection& sortMap(const MapCollection &other_);
+    MapCollection();                                      
+    MapCollection(const MapCollection &other_);
+    MapCollection& operator=(const MapCollection &other_) = default;  
+    MapCollection(MapCollection &&other_) = default;  
+    MapCollection& operator=(MapCollection &&other_) = default;  
+    void clear();                                         
+    size_t size() const;                                  
+    void readFromFile(std::string filename);              
+    void writeToFile(std::string filename) const;        
+    bool operator==(const MapCollection &other_);   
+    bool compareMaps(const MapCollection &other_); 
+    void insert(const K& key_, const V& value_);          
+    void erase(const K& key_);                            
+    V &find(const K& key_);                              
+    V &operator[](const K& key_);                        
+    bool exist(const K& key_) const;                      
+    std::vector<V> allValuesReturn(const MapCollection &other_);               
+    bool compareKeys(const K& key_);                       
+    bool operator>(const K& key_);                          
     ~MapCollection() = default;
 private:
     std::vector<K> m_keys;
